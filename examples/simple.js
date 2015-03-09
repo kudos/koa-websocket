41700988e9ada38be0b1f8e005433bdc95e2e9c6
Jonathan Cremin <jonathan@crem.in>
2015-03-08 18:06:15 -0700

    Add CI

--- examples/simple.js
+++ examples/simple.js
@@ -8,8 +8,8 @@ const app = websockify(koa());
 
 // Note it's app.ws.use and not app.use
 app.ws.use(route.all('/test/:id', function* (next) {
-  this.send('Hello World');
   // `this` is the socket passed from WebSocketServer to connection listeners
+  this.send('Hello World');
   this.on('message', function(message) {
     // do something with the message from client
   });
