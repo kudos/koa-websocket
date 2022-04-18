99289c86bbc839b102d5a7e3be663a6af7d6a843
Jonathan Cremin <jonathan@crem.in>
2022-04-18 17:12:18 +0100

    Deprecate koa-route, update examples

--- README.md
+++ README.md
@@ -12,33 +12,9 @@ Supports `ws://` and `wss://`
 
 ## Usage
 
-```js
-const Koa = require('koa'),
-  route = require('koa-route'),
-  websockify = require('koa-websocket');
+See examples directory for a simple implementation.
 
-const app = websockify(new Koa());
-
-// Regular middleware
-// Note it's app.ws.use and not app.use
-app.ws.use(function(ctx, next) {
-  // return `next` to pass the context (ctx) on to the next ws middleware
-  return next(ctx);
-});
-
-// Using routes
-app.ws.use(route.all('/test/:id', function (ctx) {
-  // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
-  // the websocket is added to the context on `ctx.websocket`.
-  ctx.websocket.send('Hello World');
-  ctx.websocket.on('message', function(message) {
-    // do something with the message from client
-        console.log(message);
-  });
-}));
-
-app.listen(3000);
-```
+### Let's Encrypt
 
 Example with Let's Encrypt ([the Greenlock package](https://git.daplie.com/Daplie/greenlock-koa)):
 
@@ -61,27 +37,6 @@ app.ws.use((ctx) => {
   });
 });
  
-app.listen(3000);
-```
-
-With custom websocket options.
-
-```js
-const Koa = require('koa'),
-  route = require('koa-route'),
-  websockify = require('koa-websocket');
-
-const wsOptions = {};
-const app = websockify(new Koa(), wsOptions);
-
-app.ws.use(route.all('/', (ctx) => {
-   // the websocket is added to the context as `ctx.websocket`.
-  ctx.websocket.on('message', function(message) {
-    // print message from the client
-    console.log(message);
-  });
-}));
-
 app.listen(3000);
 ```
 
--- examples/simple.js
+++ examples/simple.js
@@ -1,21 +1,44 @@
-const koa = require('koa');
-const route = require('koa-route');
+const Koa = require('koa');
+const Router = require('@koa/router');
 const websockify = require('..');
 
-const app = websockify(koa());
+const app = websockify(new Koa());
 
-// Note it's app.ws.use and not app.use
-app.ws.use(route.all('/test/:id', function* all(next) {
-  // `this` is the regular koa context created from the `ws`
-  // onConnection `socket.upgradeReq` object.
-  // The websocket is added to the context on `this.websocket`.
-  this.websocket.send('Hello World');
-  this.websocket.on('message', (message) => {
-    // Do something with the message from client
-    console.log(message);
+const wsRouter = Router();
+const router = Router();
+
+// Regular middleware
+wsRouter.use((ctx, next) => {
+  // return `next` to pass the context (ctx) on to the next ws middleware
+  return next(ctx);
+});
+
+// Using routes
+wsRouter.get('/', async (ctx, next) => {
+  // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+  // the websocket is added to the context on `ctx.websocket`.
+  ctx.websocket.send('Hello World');
+  ctx.websocket.on('message', (message) => {
+    // do something with the message from client
+    if (message.toString() === 'ping') {
+      ctx.websocket.send('pong');
+    }
   });
-  // Yielding `next` will pass the context (this) on to the next ws middleware
-  yield next;
-}));
+  return next;
+});
+
+// A normal http route to set up a basic ws test
+router.get('/', async (ctx, next) => {
+  ctx.body = `<script>
+    ws = new WebSocket("ws://localhost:3000");
+    ws.addEventListener('open', () => { ws.send("ping"); });
+  </script>Check the network inspector!`;
+  return next;
+});
+
+// Attach both routers
+// Note it's app.ws.use for our ws router
+app.ws.use(wsRouter.routes()).use(wsRouter.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
